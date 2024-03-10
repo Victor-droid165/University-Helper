39c9cc9ce1dc052568622407a49cdde41b866f51
Victor-droid165 <victor.emanuel.barbosa.rodrigues@ccc.ufcg.edu.br>
2024-03-10 14:47:36 -0300

    fix: added content field to Note data structure

--- src/Notes.hs
+++ src/Notes.hs
@@ -1,31 +1,32 @@
 module Notes where
 
+import qualified Data.HashMap.Strict as HashMap
+import Data.IORef
+import Data.Time.Clock
+import Data.Time.LocalTime
+import System.IO.Unsafe (unsafePerformIO)
 import Types
 
-import Data.Time.LocalTime
-import Data.Time.Clock
-import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
-import qualified Data.HashMap.Strict as HashMap
-
 data Note = Note
-  { noteType :: NoteType
-  , noteId :: String
-  , visibility :: Visibility
-  , createdAt :: LocalTime
-  , lastEditedAt :: LocalTime
-  , title :: Maybe String
-  , subject :: Maybe String
-  } deriving (Show)
+  { noteType :: NoteType,
+    noteId :: String,
+    content :: String,
+    visibility :: Visibility,
+    createdAt :: LocalTime,
+    lastEditedAt :: LocalTime,
+    title :: Maybe String,
+    subject :: Maybe String
+  }
+  deriving (Show)
 
 -- Função para criar uma nova anotação
-createNote :: NoteType -> Visibility -> Maybe String -> Maybe String -> IO Note
-createNote noteType' visibility' title' subject' = do
-    timeZone        <- getCurrentTimeZone
-    currentUTCTime  <- getCurrentTime
-    noteId'         <- generateId noteType'
-    let currentLocalTime = utcToLocalTime timeZone currentUTCTime
-    return $ Note noteType' noteId' visibility' currentLocalTime currentLocalTime title' subject'
+createNote :: NoteType -> String -> Visibility -> Maybe String -> Maybe String -> IO Note
+createNote noteType' content' visibility' title' subject' = do
+  timeZone <- getCurrentTimeZone
+  currentUTCTime <- getCurrentTime
+  noteId' <- generateId noteType'
+  let currentLocalTime = utcToLocalTime timeZone currentUTCTime
+  return $ Note noteType' noteId' content' visibility' currentLocalTime currentLocalTime title' subject'
 
 -- Definição de um tipo Map de string para inteiro
 type NoteCounts = HashMap.HashMap String Int
@@ -40,7 +41,7 @@ incrementNoteCount noteType' = atomicModifyIORef' nextNotesIds update
   where
     update counts =
       let newCounts = HashMap.insertWith (+) (show noteType') 1 counts
-      in (newCounts, ())
+       in (newCounts, ())
 
 -- Função para recuperar a contagem de um dado tipo de anotação
 getNoteCount :: NoteType -> IO Int
@@ -50,12 +51,12 @@ getNoteCount noteType' = do
 
 -- Função para gerar o Id de uma anotação
 generateId :: NoteType -> IO String
-generateId noteType' = do 
-    num <- getNextNoteId noteType'
-    return $ prefix noteType' ++ idSeparator ++ show num
-    where 
-        idSeparator = "-"
-        getNextNoteId noteType' = do
-            count <- getNoteCount noteType'
-            incrementNoteCount noteType'
-            return (count + 1)
+generateId noteType' = do
+  num <- getNextNoteId noteType'
+  return $ prefix noteType' ++ idSeparator ++ show num
+  where
+    idSeparator = "-"
+    getNextNoteId noteType' = do
+      count <- getNoteCount noteType'
+      incrementNoteCount noteType'
+      return (count + 1)
