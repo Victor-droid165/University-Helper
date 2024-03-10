88165f8812e22f9e3192c8717368aea73873d7a7
Victor-droid165 <victor.emanuel.barbosa.rodrigues@ccc.ufcg.edu.br>
2024-03-10 10:24:57 -0300

    feat: Added logic to generate notes ids

--- src/Notes.hs
+++ src/Notes.hs
@@ -4,6 +4,9 @@ import Types
 
 import Data.Time.LocalTime
 import Data.Time.Clock
+import Data.IORef
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.HashMap.Strict as HashMap
 
 data Note = Note
   { noteType :: NoteType
@@ -15,13 +18,38 @@ data Note = Note
   , subject :: Maybe String
   } deriving (Show)
 
-createNote :: NoteType -> String -> Visibility -> Maybe String -> Maybe String -> IO Note
-createNote noteType' noteId' visibility' title' subject' = do
-    timeZone <- getCurrentTimeZone
-    currentUTCTime <- getCurrentTime 
+-- Função para criar uma nova anotação
+createNote :: NoteType -> Visibility -> Maybe String -> Maybe String -> IO Note
+createNote noteType' visibility' title' subject' = do
+    timeZone        <- getCurrentTimeZone
+    currentUTCTime  <- getCurrentTime
+    noteId'         <- generateId noteType'
     let currentLocalTime = utcToLocalTime timeZone currentUTCTime
     return $ Note noteType' noteId' visibility' currentLocalTime currentLocalTime title' subject'
 
-generateId :: NoteType -> Int -> String
-generateId noteType' num = prefix noteType' ++ idSeparator ++ show num
-    where idSeparator = "-"
+type NoteCounts = HashMap.HashMap String Int
+nextNotesIds :: IORef NoteCounts
+nextNotesIds = unsafePerformIO (newIORef HashMap.empty)
+
+incrementNoteCount :: NoteType -> IO ()
+incrementNoteCount noteType' = atomicModifyIORef' nextNotesIds update
+  where
+    update counts =
+      let newCounts = HashMap.insertWith (+) (show noteType') 1 counts
+      in (newCounts, ())
+
+getNoteCount :: NoteType -> IO Int
+getNoteCount noteType' = do
+  counts <- readIORef nextNotesIds
+  return (HashMap.lookupDefault 0 (show noteType') counts)
+
+generateId :: NoteType -> IO String
+generateId noteType' = do 
+    num <- getNextNoteId noteType'
+    return $ prefix noteType' ++ idSeparator ++ show num
+    where 
+        idSeparator = "-"
+        getNextNoteId noteType' = do
+            count <- getNoteCount noteType'
+            incrementNoteCount noteType'
+            return (count + 1)
