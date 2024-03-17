f5ea2b6019d25eccaa8d135985855c57ea55ae5e
Victor-droid165 <victor.emanuel.barbosa.rodrigues@ccc.ufcg.edu.br>
2024-03-17 18:49:07 -0300

    refactor: making some good refactoring on getInput and select functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,15 @@
 module Lib
-    (
-      getInput,
-      getValidInput,
-      getParsedInput,
-      getParsedValidInput,
-      handleValidation,
-      handleMaybe,
-      stringToData,
-      writeDataOnFile,
-      selectOption
-    ) where
+  ( getInput,
+    getValidInput,
+    getParsedInput,
+    getParsedValidInput,
+    handleValidation,
+    handleMaybe,
+    stringToData,
+    writeDataOnFile,
+    selectOption,
+  )
+where
 
 -- Remember to import here
 import System.IO
@@ -17,45 +17,46 @@ import Text.Read (readMaybe)
 
 getInput :: Maybe String -> IO String
 getInput maybePrompt = do
-    handleMaybe maybePrompt (return ()) (\prompt -> putStr prompt)
-    hFlush stdout
-    getLine
+  handleMaybe maybePrompt (return ()) putStr
+  hFlush stdout
+  getLine
 
 getParsedInput :: (Read a) => Maybe String -> IO a
 getParsedInput maybePrompt = do
-    value <- getInput maybePrompt
-    return $ read value
+  value <- getInput maybePrompt
+  return $ read value
 
 getValidInput :: Maybe String -> (String -> FormValidation String) -> IO String
-getValidInput maybePrompt validationFunc = do 
-    value <- getInput maybePrompt
-    handleValidation (validationFunc value) (return value) (getValidInput maybePrompt validationFunc)
+getValidInput maybePrompt validationFunc = do
+  value <- getInput maybePrompt
+  handleValidation (validationFunc value) (return value) (getValidInput maybePrompt validationFunc)
 
 getParsedValidInput :: (Read a) => Maybe String -> (String -> FormValidation String) -> IO a
 getParsedValidInput maybePrompt validationFunc = do
-    value <- getValidInput maybePrompt validationFunc
-    return $ read value
+  value <- getValidInput maybePrompt validationFunc
+  return $ read value
 
 handleValidation :: FormValidation String -> IO String -> IO String -> IO String
 handleValidation (Failure err) _ actionIfFailure = do
-    clearScreen
-    putStrLn $ "Erro: " ++ show err
-    actionIfFailure
+  clearScreen
+  putStrLn $ "Erro: " ++ show err
+  actionIfFailure
 handleValidation _ actionIfSuccess _ = do actionIfSuccess
 
-stringToData :: Read a => String -> Maybe a
+stringToData :: (Read a) => String -> Maybe a
 stringToData str = readMaybe str
 
-writeDataOnFile :: Show a => FilePath -> a -> IO ()
+writeDataOnFile :: (Show a) => FilePath -> a -> IO ()
 writeDataOnFile filePath data' = appendFile filePath (show data' ++ "\n")
 
 handleMaybe :: Maybe a -> b -> (a -> b) -> b
 handleMaybe (Just value) _ function = function value
 handleMaybe _ defaultValue _ = defaultValue
 
-selectOption :: [String] -> IO String
+selectOption :: [(String, IO ())] -> IO ()
 selectOption options = do
-  displayOptions options
+  let optionsPrompts = map fst options
+  displayOptions optionsPrompts
   choice <- getUserChoice
   handleChoice choice options
 
@@ -78,17 +79,18 @@ getUserChoice = do
   hFlush stdout
   getLine
 
-handleChoice :: String -> [String] -> IO String
+handleChoice :: String -> [(String, IO ())] -> IO ()
+handleChoice "." options = snd (options !! ((length options) - 1))
 handleChoice choice options
   | Just index <- readMaybe choice :: Maybe Int,
-    isValidIndex index options =
-      return (options !! (index - 1))
+    isValidIndex index 1 (length options) =
+      snd (options !! (index - 1))
   | otherwise = retryChoice options
 
-isValidIndex :: Int -> [a] -> Bool
-isValidIndex index options = index >= 1 && index <= length options
+isValidIndex :: Int -> Int -> Int -> Bool
+isValidIndex index minIndex maxIndex = index >= minIndex && index <= maxIndex
 
-retryChoice :: [String] -> IO String
+retryChoice :: [(String, IO ())] -> IO ()
 retryChoice options = do
   putStrLn "Opcao Invalida. Tente Novamente."
   putStrLn "Se deseja escolher a opcao '[X] - Opcao', digite: X"
