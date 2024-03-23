70d3c848e579f8b782e565494f044ab22625008f
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-23 00:11:35 -0300

    update: adding warnings on login

--- frontend/src/components/UserLoginForm/UserLoginForm.js
+++ frontend/src/components/UserLoginForm/UserLoginForm.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container } from '@mui/material';
+import { Avatar, Button, CssBaseline, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import Alert from '@mui/material/Alert';
 
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
@@ -42,23 +42,21 @@ const UserLoginForm = () => {
       passwordError: '',
     });
 
-    useEffect(() => {
-        if(Object.keys(errors).length === 0){
-            console.log(JSON.stringify(logInfo))
-            const response = fetch(`http://localhost:8081/userLogin`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(logInfo)
-            }).then(response => response.json()).then((json) => {
-                console.log(json);
+    const [alerts, setAlerts] = useState([]);
 
-            }).catch((error) => {
-                console.error('Error: ', error);
-            });
+    useEffect(() => {
+        if (alerts.length > 0) {
+            const timer = setTimeout(() => {
+                setAlerts(prevAlerts => prevAlerts.slice(1));
+            }, 3000);
+
+            return () => clearTimeout(timer);
         }
-    },[errors])
+    }, [alerts]);
+
+    const showAlert = (severity, message) => {
+        setAlerts(prevAlerts => [...prevAlerts, { severity, message }]);
+    };
 
     const handleChange = (e) => {
         setLogInfo({
@@ -69,7 +67,7 @@ const UserLoginForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+    
         try {
             const validationResults = await validateUser(logInfo);
             const validationErrors = validationResults.reduce((errors, error, index) => {
@@ -79,6 +77,19 @@ const UserLoginForm = () => {
                 return errors;
             }, {});
             setErrors(validationErrors);
+            if (Object.keys(validationErrors).length === 0) {
+                const response = await fetch(`http://localhost:8081/userLogin`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(logInfo)
+                });
+                const json = await response.json();
+                if (!json) {
+                    showAlert('error', 'Login e/ou senha inválidos');
+                }
+            }
         } catch (error) {
             console.error('Erro:', error);
         }
@@ -110,76 +121,91 @@ const UserLoginForm = () => {
             mt: 3,
             mb: 2,
         },
+        alertContainer: {
+            position: 'fixed',
+            bottom: '2%',
+            right: '1%',
+            zIndex: 9999,
+        },
     };
 
     return (
-        <Box sx={styles.form}>
-            <ThemeProvider theme={createTheme()}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                            <LockOutlinedIcon />
-                        </Avatar>
-                        <Typography component="h1" variant="h5">
-                            Sign in
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
-                                autoFocus
-                                sx={styles.textField}
-                                value={logInfo.email}
-                                onChange={handleChange}
-                                error={Boolean(errors.emailError)}
-                                helperText={errors.emailError}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
-                                sx={styles.textField}
-                                value={logInfo.password}
-                                onChange={handleChange}
-                                error={Boolean(errors.passwordError)}
-                                helperText={errors.passwordError}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ ...styles.button, mt: 3, mb: 2 }}
-                            >
-                                Sign In
-                            </Button>
-                            <Grid container justifyContent="flex-end">
-                                <Grid item>
-                                    <Link href="/register" variant="body2">
-                                        Don't have an account? Sign Up
-                                    </Link>
+        <Box>
+            <Box sx={styles.form}>
+                <ThemeProvider theme={createTheme()}>
+                    <Container component="main" maxWidth="xs">
+                        <CssBaseline />
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                                <LockOutlinedIcon />
+                            </Avatar>
+                            <Typography component="h1" variant="h5">
+                                Sign in
+                            </Typography>
+                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    autoFocus
+                                    sx={styles.textField}
+                                    value={logInfo.email}
+                                    onChange={handleChange}
+                                    error={Boolean(errors.emailError)}
+                                    helperText={errors.emailError}
+                                />
+                                <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    sx={styles.textField}
+                                    value={logInfo.password}
+                                    onChange={handleChange}
+                                    error={Boolean(errors.passwordError)}
+                                    helperText={errors.passwordError}
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ ...styles.button, mt: 3, mb: 2 }}
+                                >
+                                    Sign In
+                                </Button>
+                                <Grid container justifyContent="flex-end">
+                                    <Grid item>
+                                        <Link href="/register" variant="body2">
+                                            Don't have an account? Sign Up
+                                        </Link>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
+                            </Box>
                         </Box>
-                    </Box>
-                </Container>
-            </ThemeProvider>
+                    </Container>
+                </ThemeProvider>
+            </Box>
+            <Box sx={styles.alertContainer}>
+                {alerts.map((alert, index) => (
+                    <Alert key={index} severity={alert.severity} sx={{ my: 1 }}>
+                        {alert.message}
+                    </Alert>
+                ))}
+            </Box>
         </Box>
     );
 };
