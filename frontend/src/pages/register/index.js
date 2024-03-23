8656b8a6785a0458b766df5e6a7c02782b89d23d
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-23 00:09:14 -0300

    refactor: register screen and errors

--- frontend/src/pages/register/index.js
+++ frontend/src/pages/register/index.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
-import { TextField, Box, Button, Select, MenuItem } from '@mui/material';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container, MenuItem } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Alert from '@mui/material/Alert';
+import { useNavigate } from 'react-router-dom';
+
 
 function Register() {
   return (
@@ -27,186 +32,261 @@ const User = () => {
     userPasswordError: '',
   });
 
-    const handleChange = (e) => {
-      setUser({
-        ...user,
-        [e.target.name]: e.target.value
+  const [alerts, setAlerts] = useState([]);
+
+  useEffect(() => {
+      if (alerts.length > 0) {
+          const timer = setTimeout(() => {
+              setAlerts(prevAlerts => prevAlerts.slice(1));
+          }, 3000);
+
+          return () => clearTimeout(timer);
+      }
+  }, [alerts]);
+
+  const showAlert = (severity, message) => {
+      setAlerts(prevAlerts => [...prevAlerts, { severity, message }]);
+  };
+
+  const handleChange = (e) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const navigate = useNavigate();
+    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    for (let field in user) {
+      if (field === "userType") continue;
+
+    fetch('http://localhost:8081/' + field, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ value: user[field] })
+
+    })
+      .then(response => response.json())
+      .then((json) => {
+          setErrors(prevErrors => ({
+            ...prevErrors,
+            [field + 'Error']: (json === 'Success') ? '' : json,
+          }))
+          if (json === 'Success') {
+            errors[field+'Error'] = '';
+          } else {
+            errors[field+'Error'] = json;
+          }
       });
-    };
-      
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      
-      for (let field in user) {
-        if (field === "userType") continue;
-
-      fetch('http://localhost:8081/' + field, {
-        method: 'POST',
+    }
+    
+    fetch('http://localhost:8081/isRegistered', {
+        method: 'Post',
         headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ value: user[field] })
-
-      })
+        body: JSON.stringify({ value: user.userEmail })
+        })
         .then(response => response.json())
         .then((json) => {
-            setErrors(prevErrors => ({
-              ...prevErrors,
-              [field + 'Error']: (json === 'Success') ? '' : json,
-            }))
-            if (json === 'Success') {
-              errors[field+'Error'] = '';
-            } else {
-              errors[field+'Error'] = json;
-            }
-        });
+          var register = true
+          for (let errorCheck in errors) {
+            if (errors[errorCheck] !== '' && errors[errorCheck] !== 'Success') {
+              register = false;
+              break;
+            } 
+          }
+          
+          if (json === 'Success' && register) {
+
+            fetch('http://localhost:8081/register', {
+              method: 'Post',
+              headers: {
+              'Content-Type': 'application/json',
+              },
+                body: JSON.stringify(user),
+              })
+
+              .then(response => response.json())
+              .then(data => {
+                console.log('Success:', data);
+              })
+              .catch((error) => {
+                console.error('Error:', error);
+              });
+              navigate('/');
+
+      } else if (json === 'Failure') {
+          showAlert('error', "Usuário já cadastrado no nosso sistema!\nFaça o login!");
       }
-      
-      fetch('http://localhost:8081/isRegistered', {
-          method: 'Post',
-          headers: {
-          'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ value: user.userEmail })
-          })
-          .then(response => response.json())
-          .then((json) => {
-            var register = true
-            for (let errorCheck in errors) {
-              console.log(errors[errorCheck]);
-              
-              if (errors[errorCheck] !== '' && errors[errorCheck] !== 'Success') {
-                console.log(errorCheck)
-                register = false;
-                break;
-              } 
-            }
-            
-            if (json === 'Success' && register) {
 
-              fetch('http://localhost:8081/register', {
-                method: 'Post',
-                headers: {
-                'Content-Type': 'application/json',
-                },
-                  body: JSON.stringify(user),
-                })
-
-                .then(response => response.json())
-                .then(data => {
-                  console.log('Success:', data);
-                })
-                .catch((error) => {
-                  console.error('Error:', error);
-                });
-
-        } else if (json === 'Failure') {
-          const Registered = () => { alert("Usuário já cadastrado no nosso sistema!\nFaça o login"); }
-          const root = ReactDOM.createRoot(document.getElementById('root'));
-          root.render(<Registered />);
-        }
-
-      })
+    })
 
 
-    };
+  };
+
+    const styles = {
+      form: {
+          width: 'auto',
+          height: 'auto',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          backgroundColor: 'white',
+          border: '1px solid rgba(0, 0, 0, 0.1)',
+          borderRadius: '8px',
+          padding: '20px',
+          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+      },
+      textField: {
+          mt: 1,
+          mb: 1,
+      },
+      button: {
+          mt: 3,
+          mb: 2,
+      },
+      alertContainer: {
+          position: 'fixed',
+          bottom: '2%',
+          right: '1%',
+          zIndex: 9999,
+      },
+  };
   
     return (
-      <Box component="form" onSubmit={handleSubmit} sx={{
-        width: '35%',
-        height: '70%',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        display: 'flex',
-        alignItems: 'center', // Alinha os itens verticalmente ao centro
-        justifyContent: 'center', // Alinha os itens horizontalmente ao centro
-        flexDirection: 'column',
-        backgroundColor: 'white', // Adiciona fundo branco
-        border: '1px solid rgba(0, 0, 0, 0.1)', // Adiciona uma borda com sombra
-        borderRadius: '8px', // Adiciona um arredondamento de borda
-        padding: '20px', // Adiciona um preenchimento interno
-        boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)', // Adiciona uma sombra
-      }}>
-        <TextField
-          label="Nome"
-          name="userName"
-
-        error={Boolean(errors.userNameError)}
-        helperText={errors.userNameError}
-
-        value={user.userName}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%" }} // Adicione a propriedade sx diretamente no TextField
-      />
-
-      <TextField
-        label="Universidade"
-        name="userUniversity"
-
-        error={Boolean(errors.userUniversityError)}
-        helperText={errors.userUniversityError}
-
-        value={user.userUniversity}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%" }} // Adicione a propriedade sx diretamente no TextField
-      />
-
-      <TextField
-        label="Email"
-        name="userEmail"
-
-        error={Boolean(errors.userEmailError)}
-        helperText={errors.userEmailError}
-
-        value={user.userEmail}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%" }} // Adicione a propriedade sx diretamente no TextField
-      />
-      <Select
-        label="Tipo de Usuário"
-        name="userType"
-        value={user.userType}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%"}} // Adicione a propriedade sx diretamente no Select
-      >
-        <MenuItem value="student">Aluno</MenuItem>
-        <MenuItem value="professor">Professor</MenuItem>
-      </Select>
-
-      <TextField
-        label="Matricula"
-        name="userEnrollment"
-
-        error={Boolean(errors.userEnrollmentError)}
-        helperText={errors.userEnrollmentError}
-
-        value={user.userEnrollment}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%" }} // Adicione a propriedade sx diretamente no TextField
-      />
-
-      <TextField
-        label="Senha"
-        name="userPassword"
-
-        error={Boolean(errors.userPasswordError)}
-        helperText={errors.userPasswordError}
-
-        value={user.userPassword}
-        onChange={handleChange}
-        fullWidth
-        sx={{ marginBottom: '4%', width: "80%" }} // Adicione a propriedade sx diretamente no TextField
-      />
-      <Button variant="contained" color="primary" type="submit" sx={{ width: "80%" }}>Cadastrar</Button>
-    </Box>
-  );
-};
+      <Box>
+      <Box sx={styles.form}>      
+        <ThemeProvider theme={createTheme()}>
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                  Sign up
+              </Typography>
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                        autoComplete="given-name"
+                        name="userName"
+                        required
+                        fullWidth
+                        id="userName"
+                        label="Nome"
+                        autoFocus
+                        error={Boolean(errors.userNameError)}
+                        helperText={errors.userNameError}
+                        value={user.userName}
+                        onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="userUniversity"
+                      label="Universidade"
+                      name="userUniversity"
+                      autoComplete="university"
+                      error={Boolean(errors.userUniversityError)}
+                      helperText={errors.userUniversityError}
+                      value={user.userUniversity}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="userEnrollment"
+                      label="Matrícula"
+                      name="userEnrollment"
+                      autoComplete="enrollment"
+                      error={Boolean(errors.userEnrollmentError)}
+                      helperText={errors.userEnrollmentError}
+                      value={user.userEnrollment}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="userEmail"
+                      label="Email Address"
+                      name="userEmail"
+                      autoComplete="email"
+                      error={Boolean(errors.userEmailError)}
+                      helperText={errors.userEmailError}
+                      value={user.userEmail}
+                      onChange={handleChange}
+                    />
+                  </Grid>
+                      <Grid item xs={12}>
+                          <TextField
+                              required
+                              fullWidth
+                              name="userPassword"
+                              label="Password"
+                              type="password"
+                              id="userPassword"
+                              autoComplete="new-password"
+                              error={Boolean(errors.userPasswordError)}
+                              helperText={errors.userPasswordError}
+                              value={user.userPassword}
+                              onChange={handleChange}
+                          />
+                      </Grid>
+                  </Grid>
+                  <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      sx={{ mt: 3, mb: 2 }}
+                  >
+                      Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                      <Grid item>
+                          <Link href="/login" variant="body2">
+                              Already have an account? Sign in
+                          </Link>
+                      </Grid>
+                  </Grid>
+              </Box>
+            </Box>
+          </Container>
+        </ThemeProvider>
+      </Box>
+        <Box sx={styles.alertContainer}>
+            {alerts.map((alert, index) => (
+                <Alert key={index} severity={alert.severity} sx={{ my: 1 }}>
+                    {alert.message}
+                </Alert>
+            ))}
+        </Box>
+      </Box>
+    );
+}
 
 export default Register;
