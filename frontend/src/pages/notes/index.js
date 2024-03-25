78128f8aea01adcc8226aa256bad089c7f908672
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-25 03:11:59 -0300

    feature: first commit from notes

---
+++ frontend/src/pages/notes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ContinuousText from './continuousText';
+
+function NoteCreationPage() {
+    return (
+        <ContinuousText />
+    );
+}
+
+export default NoteCreationPage;
