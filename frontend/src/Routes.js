6d11c9da2e3f80645240934adff4a03dc699f8e0
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-25 03:11:15 -0300

    update: new modules from MUI and new Routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,8 @@ import AppPageLayout from './layouts/AppPageLayout/AppPageLayout';
 import HomePage from './pages/home';
 import RegisterPage, { registerAction } from './pages/register';
 import LoginPage from './pages/login';
+import AdminPage from './pages/admin';
+import NoteCreationPage from './pages/notes';
 import { loginAction, logoutAction } from './components/UserLoginForm/UserLoginForm';
 
 const router = createBrowserRouter(
@@ -11,9 +13,12 @@ const router = createBrowserRouter(
     <Route path='/' element={<AppPageLayout />}>
       <Route index element={<HomePage />} />
       <Route path="home" element={<HomePage />} />
+      <Route path="admin" element={<AdminPage />} />
+      <Route path="note_creation" element={<NoteCreationPage />} />
       <Route path="register" element={<RegisterPage />} action={registerAction}/>
       <Route path="login" element={<LoginPage />} action={loginAction}/>
       <Route path="logout" action={logoutAction}/>
+      {/* <Route path="admin" element={<Album />}/> */}
 
       {/*<Route path='protected' element={<RequireAuthh> <ProtectedComponent></ProtectedComponent></RequireAuth>}/>*/}
       {/*<Route path='*' element={<NotFoundPage />}/>*/}
