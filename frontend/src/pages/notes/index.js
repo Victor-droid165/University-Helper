bc5a82f7b0486b7fd21d1abfe9d98c9afd23d7aa
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-26 00:47:01 -0300

    feature: note creation pages(notice, reminder and text)

--- frontend/src/pages/notes/index.js
+++ frontend/src/pages/notes/index.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import ContinuousText from './continuousText';
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import WarningIcon from '@mui/icons-material/Warning';
+import DescriptionIcon from '@mui/icons-material/Description';
+import LightbulbIcon from '@mui/icons-material/Lightbulb';
+import TextNote from './textNote';
+import { blue, red } from "@mui/material/colors";
+import Notice from './notice';
+import Reminder from './reminder';
 
-function NoteCreationPage() {
-    return (
-        <ContinuousText />
-    );
+export default function IconPositionTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const getContent = (index) => {
+    switch (index) {
+      case 0:
+        return <Notice />;
+      case 1:
+        return <TextNote />;
+      case 2:
+        return <Reminder />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box sx={{ p: 3, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', borderRadius: '16px' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          textColor="primary"
+          indicatorColor="primary"
+          aria-label="icon position tabs example"
+          sx={{
+            '.MuiTabs-indicator': {
+              backgroundColor: red[500], // Cor do indicador personalizada
+            },
+            '.MuiTabs-flexContainer': {
+              bgcolor: blue[800], // Cor de fundo azul para os Tabs
+            },
+            '.MuiTab-root': {
+              color: 'white', // Cor do texto dos Tabs
+              '&.Mui-selected': { // Estilos para o Tab selecionado
+                color: blue[50], // Cor do texto mais clara para o Tab selecionado
+              },
+            },
+          }}
+        >
+          <Tab icon={<WarningIcon />} label="Advertência" />
+          <Tab icon={<DescriptionIcon />} label="Texto Corrido" />
+          <Tab icon={<LightbulbIcon />} label="Lembretes" />
+        </Tabs>
+        <Box sx={{ p: 5, borderRadius: '16px' }}>
+          {getContent(value)}
+        </Box>
+      </Box>
+    </Box>
+  );
 }
-
-export default NoteCreationPage;
--- frontend/src/pages/notes/textNote.js
+++ frontend/src/pages/notes/textNote.js
@@ -2,27 +2,46 @@ import React, { useState } from 'react';
 import { Button, TextField, Typography, Container, Grid, IconButton, Checkbox, FormControlLabel } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 
-const Reminder = () => {
+const TextNote = () => {
+  const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [isPublic, setIsPublic] = useState(false);
 
   const handleSave = () => {
     const now = new Date();
     console.log("Data e Hora:", now.toLocaleString());
+    console.log("Título:", title);
     console.log("Conteúdo:", content);
+    console.log("É público:", isPublic);
     // Aqui você pode adicionar lógica para salvar no backend, se necessário
   };
 
   const handleClear = () => {
+    setTitle('');
     setContent('');
+    setIsPublic(false);
   };
 
   return (
     <Container component="main" maxWidth='100%'>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Typography component="h1" variant="h5" style={{ marginBottom: '1rem' }}>
-          Criar Lembrete
+          Criar Anotação
         </Typography>
         <form noValidate style={{ width: '100%' }}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="title"
+            label="Título"
+            name="title"
+            autoFocus
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            style={{ marginBottom: '1rem' }}
+          />
           <TextField
             variant="outlined"
             margin="normal"
@@ -38,7 +57,20 @@ const Reminder = () => {
             style={{ marginBottom: '1rem' }}
           />
           <Grid container spacing={2}>
-            <Grid item xs={12} container justifyContent="flex-end">
+            <Grid item xs={6} container justifyContent="flex-start">
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={isPublic}
+                    onChange={(e) => setIsPublic(e.target.checked)}
+                    name="isPublic"
+                    color="primary"
+                  />
+                }
+                label="Público"
+              />
+            </Grid>
+            <Grid item xs={6} container justifyContent="flex-end">
               <IconButton
                 type="button"
                 onClick={handleClear}
@@ -71,4 +103,4 @@ const Reminder = () => {
   );
 };
 
-export default Reminder;
+export default TextNote;
