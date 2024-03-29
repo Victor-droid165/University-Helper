cedf1b3de665bb2b07ceb37a4cdd8ab3d5518156
Victor-droid165 <victor.emanuel.barbosa.rodrigues@ccc.ufcg.edu.br>
2024-03-29 19:17:26 -0300

    feat: adding events and notebook tables to the app

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,5 +1,4 @@
 SET search_path TO "uh_schema";
-
 -- In our app, users must have name, email, password and a type.
 -- users can also take part on a university, thus, having an enrollment number and
 -- a field with the name of the university they attend.
@@ -10,11 +9,11 @@ CREATE TABLE users (
     type VARCHAR(10) CHECK (type IN ('Admin', 'Student', 'Professor')) NOT NULL,
     enrollment_number VARCHAR(20),
     university_name VARCHAR(100),
-    is_deleted BOOLEAN DEFAULT FALSE, -- Enables soft deletion
+    is_deleted BOOLEAN DEFAULT FALSE,
+    -- Enables soft deletion
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     id SERIAL PRIMARY KEY
 );
-
 -- Table to model the relationship: user of type ADMIN VALIDATES user of type non-admin
 CREATE TABLE admin_user_validations (
     admin_id INT NOT NULL,
@@ -23,12 +22,13 @@ CREATE TABLE admin_user_validations (
     PRIMARY KEY (admin_id, user_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
 -- In our app, Notes have an id, a type, a visibility, and a content.
 -- Also, it must have been created by a user and may have title and/or subject.
 CREATE TABLE notes (
     id VARCHAR(24) PRIMARY KEY,
-    type VARCHAR(10) CHECK (type IN ('Reminder', 'StickyNote', 'PlainText', 'Warning')) NOT NULL,
+    type VARCHAR(10) CHECK (
+        type IN ('Reminder', 'StickyNote', 'PlainText', 'Warning')
+    ) NOT NULL,
     visibility VARCHAR(10) DEFAULT 'Private' CHECK (visibility IN ('Private', 'Public')),
     title VARCHAR(255),
     subject VARCHAR(255),
@@ -38,7 +38,6 @@ CREATE TABLE notes (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
 -- In our app, users can contest the note deletion during an interval of 3 days after the action.
 -- For this we created the following:
 CREATE TABLE notes_to_delete (
@@ -46,7 +45,6 @@ CREATE TABLE notes_to_delete (
     deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE
 );
-
 -- In our app, PROFESSOR AND/OR ADMIN WARNS USERS, so the table below is for that.
 CREATE TABLE user_warnings (
     warning_id VARCHAR(24) NOT NULL,
@@ -55,21 +53,23 @@ CREATE TABLE user_warnings (
     FOREIGN KEY (warning_id) REFERENCES notes(id) ON DELETE CASCADE,
     FOREIGN KEY (warned_user_id) REFERENCES users(id) ON DELETE CASCADE
 );
-
 -- In our app, notebooks can be of three types, and all of 'em must have a name.
 CREATE TABLE notebooks (
-    type VARCHAR(15) CHECK (type IN ('Convencional', 'Cronológico', 'Mental')) NOT NULL,
+    type VARCHAR(15) CHECK (
+        type IN ('Convencional', 'Cronológico', 'Mental')
+    ) NOT NULL,
     name VARCHAR(100) UNIQUE NOT NULL,
     id SERIAL PRIMARY KEY
 );
-
--- In our app, 'Convencional' notebooks are divided into subjects. Plus,
--- notes can also be labeled with many subjects. For this, we created the table below.
-CREATE TABLE subjects (
-    name VARCHAR(100) UNIQUE NOT NULL,
-    id SERIAL PRIMARY KEY
+-- In our app, 'Convencional' notebooks are divided into subjects. For this, we created the table below.
+CREATE TABLE notebook_subjects (
+    conventional_notebook_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    start_page_number INT NOT NULL,
+    end_page_number INT NOT NULL,
+    PRIMARY KEY (conventional_notebook_id, name),
+    FOREIGN KEY (conventional_notebook_id) REFERENCES notebooks(id) ON DELETE CASCADE
 );
-
 -- In our app, 'Convencional' and 'Chronological' (Normal) notebooks can have pages.
 -- These pages have a number and are part of a notebook.
 CREATE TABLE pages (
@@ -78,13 +78,11 @@ CREATE TABLE pages (
     id SERIAL PRIMARY KEY,
     FOREIGN KEY (normal_notebook_id) REFERENCES notebooks(id) ON DELETE CASCADE
 );
-
 -- Table to represent mental notebook nodes
 CREATE TABLE mental_notebook_nodes (
     label VARCHAR(50) NOT NULL,
     id SERIAL PRIMARY KEY
 );
-
 -- Table to represent the relationship between mental notebook nodes and regular notes
 CREATE TABLE mental_notebook_node_notes (
     mental_notebook_node_id INT NOT NULL,
@@ -93,7 +91,6 @@ CREATE TABLE mental_notebook_node_notes (
     FOREIGN KEY (mental_notebook_node_id) REFERENCES mental_notebook_nodes(id),
     FOREIGN KEY (note_id) REFERENCES notes(id)
 );
-
 -- Table to represent the relationship between mental notebook nodes and entire notebooks
 CREATE TABLE mental_notebook_node_notebooks (
     mental_notebook_node_id INT NOT NULL,
@@ -102,7 +99,6 @@ CREATE TABLE mental_notebook_node_notebooks (
     FOREIGN KEY (mental_notebook_node_id) REFERENCES mental_notebook_nodes(id),
     FOREIGN KEY (notebook_id) REFERENCES notebooks(id)
 );
-
 -- Table to represent connections between mental notebook nodes
 CREATE TABLE mental_notebook_node_connections (
     source_node_id INT NOT NULL,
@@ -112,17 +108,6 @@ CREATE TABLE mental_notebook_node_connections (
     FOREIGN KEY (source_node_id) REFERENCES mental_notebook_nodes(id),
     FOREIGN KEY (target_node_id) REFERENCES mental_notebook_nodes(id)
 );
-
--- A note can be labeled with many subjects. A subject can label many notes.
--- For this, we created the table below.
-CREATE TABLE note_subject (
-    note_id VARCHAR(24) NOT NULL,
-    subject_id INT NOT NULL,
-    PRIMARY KEY (note_id, subject_id),
-    FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE,
-    FOREIGN KEY (subject_id) REFERENCES subjects(id) ON DELETE CASCADE
-);
-
 -- A note may be either written or copied into a page. Either way, it will
 -- take part in one or more pages either completely or partially. For this,
 -- we created the following:
@@ -135,7 +120,6 @@ CREATE TABLE note_page (
     FOREIGN KEY (page_id) REFERENCES pages(id) ON DELETE CASCADE,
     FOREIGN KEY (note_id) REFERENCES notes(id) ON DELETE CASCADE
 );
-
 -- A note can be contested by an user. If so, the user will send a comment
 -- and this will be resolved by an admin.
 CREATE TABLE note_contestations (
@@ -149,4 +133,33 @@ CREATE TABLE note_contestations (
     FOREIGN KEY (note_id) REFERENCES notes(id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (resolved_by) REFERENCES users(id)
+);
+CREATE TABLE events (
+    type VARCHAR(30) NOT NULL CHECK (
+        type IN (
+            'In Class',
+            'Extracurricular Activities',
+            'Normal Event',
+            'Rest Record',
+            'Reading Record',
+            'Creation Record',
+            'At Work'
+        )
+    ),
+    title VARCHAR(255) NOT NULL,
+    visibility VARCHAR(10) CHECK (visibility IN ('Private', 'Public')),
+    user_id INT NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP,
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+CREATE TABLE event_schedules (
+    frequency_value INT NOT NULL,
+    frequency_unit VARCHAR(20) NOT NULL CHECK (
+        frequency_unit IN ('hour', 'day', 'week', 'month', 'year')
+    ),
+    event_id INT NOT NULL,
+    schedule_id SERIAL PRIMARY KEY,
+    FOREIGN KEY (event_id) REFERENCES events(id) ON DELETE CASCADE
 );
