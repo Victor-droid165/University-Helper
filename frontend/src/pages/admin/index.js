65315cb780876bb5642e1032f9058ed782489fbd
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-03-30 01:13:27 -0300

    feat: list of warnings and creation on admin page

--- frontend/src/pages/admin/index.js
+++ frontend/src/pages/admin/index.js
@@ -1,290 +1,124 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Box, Button, Typography, MenuItem, Select, ListItemIcon, FormControl, IconButton } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { mockDataTeam } from "../../data/mockData.js";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import DeleteIcon from "@mui/icons-material/Delete";
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import WarningIcon from '@mui/icons-material/Warning';
+import DescriptionIcon from '@mui/icons-material/Description';
+import ListUsers from './listUsers';
+import ListNotes from '../notes/list';
+// import TextNote from './textNote';
 import { blue, red } from "@mui/material/colors";
-import { useApi } from "../../hooks/useApi.js";
+import Warning from '../notes/creation/warning';
+import { mockAdminWarnings } from '../../data/mockData';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Icon from '@mui/material/Icon';
+import AddIcon from '@mui/icons-material/Add';
+// import Warning from './warning';
+// import Reminder from './reminder';
 
-const AdminPage = () => {
-  const [rows, setRows] = useState([]);
-  const api = useApi();
+export default function IconPositionTabs() {
+  const [value, setValue] = React.useState(0);
 
-  useEffect(() => {
-    api.getDBUsers().then((dbUsers) => {
-      const activeUsers = dbUsers.filter(user => user.dbIsDeleted !== true);
-      console.log(activeUsers);
-      setRows(activeUsers);
-    });
-    setValidates();
-  }, []);
-
-  const updateUser = async (field, newValue, match, matchValue) => {
-    await api.updateUserField({ field, newValue, match, matchValue });
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
-  const setValidates = () => {
-    fetch('http://localhost:8081/api/users/getIdsValidated', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    })
-      .then(response => response.json())
-      .then(data => {
-        const userIds = data.map(item => item.userId);
-        const hashSet = new Set(userIds);
-        setRows(
-          nRows => nRows.map(row => ({
-              ...row,
-              isValidated: hashSet.has(row.dbUserId), 
-            }))
+  // Função para abrir o modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  
+  const getContent = (index) => {
+    switch (index) {
+      case 0:
+        return <ListUsers />;
+      case 1:
+        return (
+          <>
+            <Box sx={{ mt: '0.7%', mr: '0.7%', display: 'flex', justifyContent: "flex-end"}}>
+              <Button onClick={handleOpen} 
+                      sx={{ 
+                        bgcolor: 'red',
+                        color: 'white',
+                        fontFamily: 'Arial, sans-serif', // Escolha a família da fonte desejada
+                        fontSize: '80%', // Ajuste o tamanho da fonte conforme necessário
+                        fontWeight: 'bold', // Defina o peso da fonte
+                        '&:hover': {
+                          bgcolor: 'darkred', // Cor de fundo ao passar o mouse
+                        }
+                      }}>
+                <AddIcon sx={{marginRight:1}}/> 
+                NOVA ADVERTENCIA 
+              </Button>
+            </Box>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                overflow: 'auto',
+              }}
+            >
+              <Box sx={{
+                bgcolor: 'white',
+                p: 4,
+                borderRadius: 2,
+                boxShadow: 24,
+                maxWidth: '40%',
+                maxHeight: '80%',
+                overflow: 'auto'
+              }}>
+                <Warning />
+              </Box>
+            </Modal>
+            <ListNotes data={mockAdminWarnings} />
+          </>
         );
-        console.log(hashSet);
-      })
-      .catch(error => {
-        console.error('Error updating user:', error);
-      });
-  };
-
-  const theme = createTheme({
-    components: {
-      MuiDataGrid: {
-        styleOverrides: {
-          columnHeader: {
-            backgroundColor: blue[800], // Cor de fundo do cabeçalho
-            color: '#ffffff', // Cor do texto no cabeçalho
-          },
-        },
-      },
-    },
-    palette: {
-      black: {
-        main: "#000000", // Cor primária
-      },
-      primary: {
-        main: "#ffffff", // Cor primária
-      },
-      secondary: {
-        main: blue[100], // Cor secundária
-      },
-      special: {
-        main: blue["300"], // Cor secundária
-      },
-      error: {
-        main: red[600], // Cor de erro
-      },
-      // Adicione outras cores conforme necessário
-    },
-  });
-
-  const handleAccessChange = (dbUserId, newAccess, email) => {
-    // Impede a alteração do nível de acesso se for 'admin'
-    const currentAccess = rows.find(row => row.dbUserId === dbUserId).dbUserType;
-    if (currentAccess !== "Admin") {
-      const newRows = rows.map((row) => {
-        if (row.dbUserId === dbUserId) {
-          return { ...row, dbUserType: newAccess };
-        }
-        return row;
-      });
-      updateUser("type", newAccess, "email", email);
-      setRows(newRows);
+      // case 2:
+      //   return <Reminder />;
+      default:
+        return null;
     }
   };
-
-
-  const handleDeleteRow = (dbUserId) => {
-    fetch('http://localhost:8081/api/users/deleteUser', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ value: dbUserId.toString() }),
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to delete user');
-        }
-        if (response === "Success") {
-          setRows(prevRows => prevRows.filter(row => row.dbUserId !== dbUserId));
-        } 
-      })
-      .catch(error => console.error('Error deleting user:', error));
-  };
-
-  const columns = [
-    { field: 'dbUserId', headerName: 'Id', headerAlign: 'center', flex: 0.5 },
-    {
-      field: 'dbUserName',
-      headerName: 'Nome',
-      headerAlign: 'center',
-      flex: 1,
-    },
-    {
-      field: 'dbUserUniversity',
-      headerName: 'Universidade',
-      type: 'number',
-      headerAlign: 'center',
-      align: 'left',
-      flex: 1,
-    },
-    { field: 'dbUserEnrollment', headerName: 'Matrícula', headerAlign: 'center', align: 'center', flex: 1 },
-    { field: 'dbUserEmail', headerName: 'Email', headerAlign: 'center', flex: 1 },
-    {
-      field: 'delete',
-      headerName: 'Deletar',
-      headerAlign: 'center',
-      flex: 0.5,
-      renderCell: (params) => (
-        <IconButton onClick={() => handleDeleteRow(params.row.dbUserId)}>
-          <DeleteIcon color="error" />
-        </IconButton>
-      ),
-    },
-    {
-      field: 'validate',
-      headerName: 'Status',
-      headerAlign: 'center',
-      align: 'center',
-      flex: 1,
-      renderCell: (params) => {
-        // O ideal é usar o padrão abaixo igual nos outros casos, mas por hora, deixarei mockado
-        const isValidated = params.row.isValidated;
-
-        //const isValidated = true;
-
-        if (!isValidated) {
-          // Botão vermelho para validar
-          return (
-            <Button
-              onClick={() => {
-                fetch('http://localhost:8081/api/users/validateUser', {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ integerValue: params.row.dbUserId }),
-                }           
-                )
-                console.log('Validar usuário', params.row.dbUserId) 
-                params.row.isValidated = true;
-                }
-              }
-              style={{ color: 'white', backgroundColor: 'red', padding: '3px 10px', borderRadius: '4px' }}
-            >
-              Validar
-            </Button>
-          );
-        } else {
-          // Texto estilizado para indicar que o usuário foi validado
-          return (
-            <Button
-              style={{ color: 'white', backgroundColor: 'green', padding: '3px 10px', borderRadius: '4px' }}
-            >
-              Validado
-            </Button>
-          );
-        }
-      }
-    },
-    {
-      field: 'dbUserType',
-      headerName: 'Cargo',
-      headerAlign: 'center',
-      flex: 1,
-      renderCell: (params) => (
-        <FormControl fullWidth>
-          <Select
-            value={params.row.dbUserType}
-            onChange={(event) => handleAccessChange(params.row.dbUserId, event.target.value, params.row.dbUserEmail)}
-            displayEmpty
-            size="small"
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              backgroundColor: params.row.dbUserType === 'admin'
-                ? theme.palette.special.main
-                : theme.palette.secondary.main,
-              color: theme.palette.primary.contrastText,
-            }}
-          >
-            <MenuItem value="Admin" disabled>
-              <ListItemIcon>
-                <AdminPanelSettingsOutlinedIcon fontSize="small" />
-              </ListItemIcon>
-              <Typography variant="body2">Admin</Typography>
-            </MenuItem>
-            <MenuItem value="Professor">
-              <ListItemIcon>
-                <SecurityOutlinedIcon fontSize="small" />
-              </ListItemIcon>
-              <Typography variant="body2">Professor</Typography>
-            </MenuItem>
-            <MenuItem value="Student">
-              <ListItemIcon>
-                <LockOpenOutlinedIcon fontSize="small" />
-              </ListItemIcon>
-              <Typography variant="body2">Aluno</Typography>
-            </MenuItem>
-          </Select>
-        </FormControl>
-      ),
-    },
-  ];
+  
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box padding={5} margin="0 auto" width={"95%"} justifyContent={"center"} alignItems={"center"}>
-        <Box
-          height="80vh"
+    <Box sx={{ p: 3, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', borderRadius: '16px' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="fullWidth"
+          textColor="primary"
+          indicatorColor="primary"
+          aria-label="icon position tabs example"
           sx={{
-            "& .MuiSelect-select": {
-              display: 'flex',
-              alignItems: 'center',
-              pl: 1,
-              pr: '24px',
+            '.MuiTabs-indicator': {
+              backgroundColor: red[500], // Cor do indicador personalizada
             },
-            "& .MuiDataGrid-root": {
-              border: "none",
+            '.MuiTabs-flexContainer': {
+              bgcolor: blue[800], // Cor de fundo azul para os Tabs
             },
-            "& .MuiDataGrid-cell": {
-              borderBottom: "none",
-            },
-            "& .name-column--cell": {
-              color: theme.palette.black.main,
-              backgroundColor: `${theme.palette.primary.main} !important`,
-            },
-            "& .MuiDataGrid-columnHeaders": {
-              backgroundColor: `${theme.palette.primary.main} !important`,
-              borderBottom: "none",
-            },
-            "& .MuiDataGrid-virtualScroller": {
-              backgroundColor: theme.palette.primary.main,
-            },
-            "& .MuiDataGrid-footerContainer": {
-              borderTop: "none",
-              backgroundColor: theme.palette.primary.main,
-            },
-            "& .MuiCheckbox-root": {
-              color: `${theme.palette.primary.main} !important`,
+            '.MuiTab-root': {
+              color: 'white', // Cor do texto dos Tabs
+              '&.Mui-selected': { // Estilos para o Tab selecionado
+                color: blue[50], // Cor do texto mais clara para o Tab selecionado
+              },
             },
           }}
         >
-          <DataGrid rows={rows}
-            columns={columns}
-            getRowId={(row) => row.dbUserId}
-            autoPageSize />
+          <Tab icon={<DescriptionIcon />} label="Lista de Usuários" />
+          <Tab icon={<WarningIcon />} label="Advertências" />
+        </Tabs>
+        <Box>
+          {getContent(value)}
         </Box>
       </Box>
-    </ThemeProvider>
+    </Box>
   );
-};
-
-export default AdminPage;
+}
